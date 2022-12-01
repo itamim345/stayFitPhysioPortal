6518be9666e3d610d97c8c70e0e4886519abbb9a
AF Tamim <itamim345@gmail.com>
2022-12-02 02:26:36 +0530

    Added code in userRoute and other changes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,42 @@
 const express = require("express");
 const router = express.Router();
-const user = require('../')
+const user = require('../Models/userModel'); //Importing Schema form Model
+const bcrypt = require('bcrypt');
+const { request } = require("express");
+const { rawListeners } = require("../Models/userModel");
 
 router.post('/login', async (req,res) => {
     try {
-        
+        const userExist = await user.findOne({
+            email: req.body.email
+        });
+        if(userExist){
+            return res.status(200).send({
+                message: "User Exists..",
+                success: false
+            })
+        }
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10); //Salted from bcrypt
+        const hashedPassword = await bcrypt.hash(password,salt); //Hashed the password
+
+        req.body.password = hashedPassword; //Converted the password to hashed format & inserted to DB
+
+        const newUser = new user(req.body);
+
+        await newUser.save();
+        res.status(200).send({
+          message: "Successfull User Creation ",
+          success: true
+        });
+
+
     } catch (error) {
-        
+        res.status(500).send({
+            message: "Error Occured!",
+            success: false, 
+            error
+        })
     }
 })
 
