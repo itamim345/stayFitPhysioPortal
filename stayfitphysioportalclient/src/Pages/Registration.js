27bd29b67d8be4770b5c9df5d85639ef3ef21d21
AF Tamim <itamim345@gmail.com>
2022-12-01 17:46:28 +0530

    Added Bootstrap and made registration page

--- stayfitphysioportalclient/src/Pages/Registration.js
+++ stayfitphysioportalclient/src/Pages/Registration.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import {Button, Form} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export default function Registration() {
-  return (
-    <div>
-      
-    </div>
-  );
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      console.log("getting user DAta", event.target.name.value);
+    }
+    return (
+      <div className="container">
+        <div className="reg-form d-flex justify-content-center align-items-center vh-100">
+          <Form onSubmit={handleSubmit} className="w-50 border border-2 border-primary p-4 rounded">
+            <h2 className="text-center text-primary text-decoration-underline">
+              Registration
+            </h2>
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Your Name" name="name" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" name="email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" name="password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Register
+            </Button>
+            <div>
+              <p className='mt-2'>
+                Already Registered? <Link to="/login">Login Here</Link>
+              </p>
+            </div>
+          </Form>
+        </div>
+      </div>
+    );
 }
