4e89abd2feaf5451c4f1ab8af9c80710d061adcc
AF Tamim <itamim345@gmail.com>
2022-12-06 02:37:38 +0530

    Edited models, added doctor,admin,seen & unseen Notification

--- Models/userModel.js
+++ Models/userModel.js
@@ -12,6 +12,22 @@ const userSchema = new mongoose.Schema({
     pass: {
         type: String,
         required: true
+    },
+    isDoctor: {
+        type: Boolean,
+        default: false
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    seenNotifications:{
+        type: Array,
+        default: []
+    },
+    unseenNotification: {
+        type: Array,
+        default: []
     }
 }, {timestamps: true});
 
