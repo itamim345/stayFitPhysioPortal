8ece4e1622f78840f21c4fca8da7ea9a76a6df83
AF Tamim <itamim345@gmail.com>
2022-12-06 01:40:56 +0530

    working with redux

--- stayfitphysioportalclient/src/Components/ProtectedRoute.js
+++ stayfitphysioportalclient/src/Components/ProtectedRoute.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import {Navigate} from 'react-router-dom';
-
+import React, { useEffect } from 'react';
+import {Navigate, useNavigate} from 'react-router-dom';
+import {useSelector} from 'react-redux';
+import axios from 'axios'
 
 export default function ProtectedRoute(props) {
+    const {user} = useSelector((state) => state.user)
+    const navigate = useNavigate();
+    const getUser = async()=> {
+        try {
+            const response = await axios.post('/api/user/get-user-info', )
+        } catch (error) {
+            
+        }
+    }
+
+    useEffect(() => {
+      if(!user){
+        getUser()
+      }
+    }, [user]);
+
     if(localStorage.getItem('token')){
         return props.children;
     }else {
