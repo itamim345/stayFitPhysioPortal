a111ecfe3c32ffa30442e093a2b67be0f08ad5e3
AF Tamim <itamim345@gmail.com>
2022-12-05 15:39:05 +0530

    Donw with protected route part

--- stayfitphysioportalclient/src/App.js
+++ stayfitphysioportalclient/src/App.js
@@ -5,6 +5,7 @@ import Registration from './Pages/Registration';
 import {Toaster} from 'react-hot-toast';
 import Home from './Pages/Home';
 import Dashboard from './Pages/Dashboard';
+import ProtectedRoute from './Components/ProtectedRoute';
 
 function App() {
   return (
@@ -14,8 +15,15 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Registration />} />
-          <Route path="/" element={<Home/>}/>
-          <Route path="/dashboard" element={<Dashboard/>}/>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
