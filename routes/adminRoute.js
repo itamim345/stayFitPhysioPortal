3633a34ec16c1c6fb23214c37a8950e88e88fed3
AF Tamim <itamim345@gmail.com>
2022-12-07 01:40:59 +0530

    Created 2 get route for admin to get all users & therapists

---
+++ routes/adminRoute.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const user = require('../Models/userModel');
+const therapist = require("../Models/therapistModel");
+
+//Get Route for getting all therapists
+router.get("/get-all-therapists", authmiddleware, async (req, res) => {
+  try {
+    const therapists = await therapist.find({})
+    return res.status(200).send({
+        message: "Therapists fetched Successfully!",
+        success: true,
+        data: doctors
+    })
+  } catch (error) {
+    return res.status(500).send({
+      message: "Error Occured in fetching Therapists",
+      success: false,
+    });
+  }
+});
+
+//Get Route for getting all users
+router.get("/get-all-users", authmiddleware, async (req, res) => {
+  try {
+    const therapists = await user.find({})
+    return res.status(200).send({
+        message: "Users fetched Successfully!",
+        success: true,
+        data: doctors
+    })
+  } catch (error) {
+    return res.status(500).send({
+      message: "Error Occured in fetching users",
+      success: false,
+    });
+  }
+});
+
+module.exports = router;
