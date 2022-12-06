cdfb48012b08959996aa4da981a92d5e13c0e5e2
AF Tamim <itamim345@gmail.com>
2022-12-06 22:01:18 +0530

    Added notification component

---
+++ stayfitphysioportalclient/src/Pages/Notifications.js
@@ -0,0 +1,10 @@
+import Layout from 'antd/es/layout/layout';
+import React from 'react';
+
+export default function Notifications() {
+  return (
+    <Layout>
+        <h3>Notification</h3>
+    </Layout>
+  );
+}
