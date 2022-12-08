e33ff03071cd884f93f8ed3c491ab475a0a4738b
AF Tamim <itamim345@gmail.com>
2022-12-08 22:12:11 +0530

    changed logo name and moved the ul to center

--- stayfitphysioportalclient/src/Pages/FrontPage/Header.js
+++ stayfitphysioportalclient/src/Pages/FrontPage/Header.js
@@ -4,13 +4,13 @@ export default function Header() {
   return (
     <div>
       <nav class="navbar navbar-expand-lg bg-light">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="#">Navbar</a>
+            <div class="container">
+              <h6>StayFit</h6>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
                   <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                   </li>
