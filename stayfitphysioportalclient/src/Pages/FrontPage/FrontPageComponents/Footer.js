8853c97349b2bf1e9c12a88e99c90f0286cb06f7
AF Tamim <itamim345@gmail.com>
2022-12-09 21:58:50 +0530

    Added footer component

---
+++ stayfitphysioportalclient/src/Pages/FrontPage/FrontPageComponents/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div>
+      <footer>
+        <p> &copy; All Right Reserved by <span>StayFit Physio Portal</span></p>
+      </footer>
+    </div>
+  );
+}
