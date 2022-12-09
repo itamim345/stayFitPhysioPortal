c6fefb8fd5b06b9d1b52b9f73b3e8deffade521f
AF Tamim <itamim345@gmail.com>
2022-12-09 16:42:32 +0530

    created WecomeArea componentd

--- stayfitphysioportalclient/src/Pages/FrontPage/FrontPageComponents/Header.js
+++ stayfitphysioportalclient/src/Pages/FrontPage/FrontPageComponents/Header.js
@@ -4,7 +4,7 @@ import '../Frontpage.css';
 
 export default function Header() {
   return (
-    <div id="header" className='bg-primary'>
+    <div id="header">
       <div class="nav-area container">
         <div className="logo">
           <h3>Logo</h3>
@@ -12,13 +12,15 @@ export default function Header() {
         <div className="menu">
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
+          <Link to="/contact">Services</Link>
+          <Link to="/about">Cotact</Link>
         </div>
         <div className="buttons">
-          <Link to="/register">
-            <button>Register</button>
+          <Link to="/login">
+            <button id="btn-1">Login</button>
           </Link>
           <Link to="/register">
-            <button>Register</button>
+            <button id="btn-2">Register</button>
           </Link>
         </div>
       </div>
---
+++ stayfitphysioportalclient/src/Pages/FrontPage/FrontPageComponents/WelcomeArea.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../Frontpage.css'
+
+export default function WelcomeArea() {
+  return (
+    <div>
+      <section className="welcome-area">
+        <div>
+          <img src="https://i.ibb.co/hm2dWfz/Banner.png" alt="welcome-img" />
+        </div>
+        <div>
+            <h2>Welcome to the StyeFit Physio Portal</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur nesciunt quidem atque earum nihil necessitatibus sapiente quo aut possimus quae.</p>
+        </div>
+      </section>
+    </div>
+  );
+}
