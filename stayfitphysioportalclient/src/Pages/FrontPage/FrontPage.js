7e2b789c94afcf8d10239e3f78002389f5ff82b9
AF Tamim <itamim345@gmail.com>
2022-12-09 21:59:23 +0530

    Added Footer component to FrontPage

--- stayfitphysioportalclient/src/Pages/FrontPage/FrontPage.js
+++ stayfitphysioportalclient/src/Pages/FrontPage/FrontPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AboutUs from './FrontPageComponents/AboutUs';
 import ContactUs from './FrontPageComponents/ContactUs';
+import Footer from './FrontPageComponents/Footer';
 import Header from './FrontPageComponents/Header';
 import HowItWorks from './FrontPageComponents/HowItWorks';
 import WelcomeArea from './FrontPageComponents/WelcomeArea';
@@ -14,6 +15,7 @@ export default function FrontPage() {
       <HowItWorks/>
       <AboutUs/>
       <ContactUs/>
+      <Footer/>
     </div>
   );
 }
