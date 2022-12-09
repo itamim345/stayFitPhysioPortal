1a5b42adf45e2a107d0604b00bee4ebf02aba2ed
AF Tamim <itamim345@gmail.com>
2022-12-09 18:04:50 +0530

    Added AboutUs component

---
+++ stayfitphysioportalclient/src/Pages/FrontPage/FrontPageComponents/AboutUs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function AboutUs() {
+  return (
+    <div>
+      <section>
+        <h2>About <span className='text-primary'>Us</span></h2>
+      </section>
+    </div>
+  );
+}
