45a4c6b68265e09a35a828ee13c843b60121abb7
AF Tamim <itamim345@gmail.com>
2022-12-09 17:34:44 +0530

    Placed demo content

--- stayfitphysioportalclient/src/Pages/FrontPage/FrontPageComponents/HowItWorks.js
+++ stayfitphysioportalclient/src/Pages/FrontPage/FrontPageComponents/HowItWorks.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 export default function HowItWorks() {
-  return (
-    <div>
-      
-    </div>
-  );
+    return (
+        <div>
+            <section>
+                <h2>How<span className='text-primary'>It Works</span></h2>
+                <div className=''>
+                    <div>
+                        <i class="ri-shield-user-line"></i>
+                        <h3>Registration</h3>
+                        <p>You have to Registration first then you only you can get the appoint or can apply as a Therapist</p>
+                        <button>Register</button>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
 }
