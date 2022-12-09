65a374baecb0ee1bcc955c916401e00749253057
AF Tamim <itamim345@gmail.com>
2022-12-09 16:43:59 +0530

    Added welcome area to fronted page

--- stayfitphysioportalclient/src/Pages/FrontPage/FrontPage.js
+++ stayfitphysioportalclient/src/Pages/FrontPage/FrontPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from './FrontPageComponents/Header';
+import WelcomeArea from './FrontPageComponents/WelcomeArea';
 export default function FrontPage() {
   return (
     <div>
       <Header/>
+      <WelcomeArea/>
     </div>
   );
 }
