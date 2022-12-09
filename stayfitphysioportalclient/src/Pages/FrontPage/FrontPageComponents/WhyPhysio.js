eda1bdc3a6d7921d3295ffb06b6f92b2457061a5
AF Tamim <itamim345@gmail.com>
2022-12-09 17:09:36 +0530

    Added content into whyphysio

--- stayfitphysioportalclient/src/Pages/FrontPage/FrontPageComponents/WhyPhysio.js
+++ stayfitphysioportalclient/src/Pages/FrontPage/FrontPageComponents/WhyPhysio.js
@@ -3,7 +3,10 @@ import React from 'react';
 export default function WhyPhysio() {
   return (
     <div>
-      
+      <section className='why-physio'>
+        <h2>Why Physio Therapy</h2>
+        <p>Physio Treatment otherwise called exercise based recuperation or kinesiology, assists with treating muscular, neurological, and cardiopulmonary infection conditions. With the assistance of PT, patients of all ages, including infants, children, adults, and the elderly, can be treated for their respective conditions. The trained allied health professionals who treat the disorder in a variety of ways are physiotherapists; thereby contributing to an improvement in the patients' overall quality of life.</p>
+      </section>
     </div>
   );
 }
