00167f645bdc3b0e794df1865b74ce46f0907302
AF Tamim <itamim345@gmail.com>
2022-12-09 17:04:45 +0530

    Added buttons in welcome area

--- stayfitphysioportalclient/src/Pages/FrontPage/FrontPageComponents/WelcomeArea.js
+++ stayfitphysioportalclient/src/Pages/FrontPage/FrontPageComponents/WelcomeArea.js
@@ -9,8 +9,11 @@ export default function WelcomeArea() {
           <img src="https://i.ibb.co/tskPkS4/welcome-img-comp.jpg" alt="welcome-img" />
         </div>
         <div>
-            <h2>Welcome to the StyeFit Physio Portal</h2>
+            <h2>Welcome to the StayFit Physio Portal</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur nesciunt quidem atque earum nihil necessitatibus sapiente quo aut possimus quae.</p>
+            <button className='btn btn-danger'>Book An Appointment</button>
+            <span className='mx-3 font-bold'>OR</span>
+            <button className='btn btn-warning'>Apply As Therapist</button>
         </div>
       </section>
     </div>
