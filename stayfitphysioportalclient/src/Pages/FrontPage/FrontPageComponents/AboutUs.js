dfbfd9d221f1b253fe44c36b164da75ae272aa8c
AF Tamim <itamim345@gmail.com>
2022-12-09 21:34:48 +0530

    almost done with aboutUs

--- stayfitphysioportalclient/src/Pages/FrontPage/FrontPageComponents/AboutUs.js
+++ stayfitphysioportalclient/src/Pages/FrontPage/FrontPageComponents/AboutUs.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function AboutUs() {
   return (
     <div>
-      <section className='container py-3 my-3 text-center'>
+      <section className='about-us container py-3 my-3 text-center'>
         <h2>About <span className='text-primary'>Us</span></h2>
         <p>We are an experience team Having a platform for the Patient and Therapist, We Ensure a excellent hassle free modern systemetic way to manage the patient and therapist problem. In this system a patient can get the appointment with any of the therapist from our platform based his choice and a therapist can apply as a therapist anytime.</p>
       </section>
