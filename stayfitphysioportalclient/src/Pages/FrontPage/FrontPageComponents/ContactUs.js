a8e27ea9ec9d6a04aa0ed920a12a998d29069a3e
AF Tamim <itamim345@gmail.com>
2022-12-09 21:53:42 +0530

    Added content into contact component

--- stayfitphysioportalclient/src/Pages/FrontPage/FrontPageComponents/ContactUs.js
+++ stayfitphysioportalclient/src/Pages/FrontPage/FrontPageComponents/ContactUs.js
@@ -3,8 +3,14 @@ import React from 'react';
 export default function ContactUs() {
   return (
     <div>
-      <section className='contact-us text-center'>
+      <section className='contact-us text-center mb-3 container'>
         <h2>Contact <span className='text-primary'>Us</span></h2>
+        <div className='contact-details py-3'>
+            <h6>StayFit PhysioPortal</h6>
+            <h6>Email: contact@stayfit.com</h6>
+            <h6>Whatsapp: +91 9889988998</h6>
+            <h6>Action Area II, Plot No.- IIA/27, Newtown, Kolkata, West Bengal 700156</h6>
+        </div>
       </section>
     </div>
   );
