257feff8995b1bb84540984589d9a7f5abba1a7b
AF Tamim <itamim345@gmail.com>
2022-12-09 21:37:23 +0530

    Added Contact us component

---
+++ stayfitphysioportalclient/src/Pages/FrontPage/FrontPageComponents/ContactUs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function ContactUs() {
+  return (
+    <div>
+      <section className='contact-us text-center'>
+        <h2>Contact <span className='text-primary'>Us</span></h2>
+      </section>
+    </div>
+  );
+}
