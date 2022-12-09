e62cb317bd752a8731ae092769b97639244eb43c
AF Tamim <itamim345@gmail.com>
2022-12-09 17:10:26 +0530

    Added whyPhysio component into frontpage

--- stayfitphysioportalclient/src/Pages/FrontPage/FrontPage.js
+++ stayfitphysioportalclient/src/Pages/FrontPage/FrontPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './FrontPageComponents/Header';
 import WelcomeArea from './FrontPageComponents/WelcomeArea';
+import WhyPhysio from './FrontPageComponents/WhyPhysio';
 export default function FrontPage() {
   return (
     <div>
       <Header/>
       <WelcomeArea/>
+      <WhyPhysio/>
     </div>
   );
 }
