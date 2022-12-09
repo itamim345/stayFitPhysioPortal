bf419ef23ca509b15eabec87c5e4b49435830352
AF Tamim <itamim345@gmail.com>
2022-12-09 17:24:53 +0530

    done with whyPhysio section

--- stayfitphysioportalclient/src/Pages/FrontPage/FrontPageComponents/WhyPhysio.js
+++ stayfitphysioportalclient/src/Pages/FrontPage/FrontPageComponents/WhyPhysio.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function WhyPhysio() {
   return (
     <div>
-      <section className='why-physio'>
-        <h2>Why Physio Therapy</h2>
+      <section className='why-physio container text-center py-3 my-3'>
+        <h2 className='mb-4'>Why <span className='text-primary'>Physio Therapy</span></h2>
         <p>Physio Treatment otherwise called exercise based recuperation or kinesiology, assists with treating muscular, neurological, and cardiopulmonary infection conditions. With the assistance of PT, patients of all ages, including infants, children, adults, and the elderly, can be treated for their respective conditions. The trained allied health professionals who treat the disorder in a variety of ways are physiotherapists; thereby contributing to an improvement in the patients' overall quality of life.</p>
       </section>
     </div>
