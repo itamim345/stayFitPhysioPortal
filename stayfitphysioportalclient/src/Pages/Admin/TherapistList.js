ff8997e1fdb1312f2752666719896c765116f372
AF Tamim <itamim345@gmail.com>
2022-12-11 19:41:59 +0530

    Added therapist list partially

--- stayfitphysioportalclient/src/Pages/Admin/TherapistList.js
+++ stayfitphysioportalclient/src/Pages/Admin/TherapistList.js
@@ -1,10 +1,68 @@
-import React from 'react';
+import { Table } from 'antd';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import DashboardLayout from '../../Components/DashboardLayout';
+import { hideLoading, showLoading } from '../../Redux/alertReducers';
 
 export default function TherapistList() {
+  const dispatch = useDispatch();
+  const [therapist, setTherapist] = useState([]); //Use State
+  //useEfftect
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
+  const getUserInfo = async () => {
+    try {
+      dispatch(showLoading());
+      const response = await axios.get("/api/admin/get-all-therapists", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      dispatch(hideLoading());
+      if (response.data.success) {
+        setTherapist(response.data.data);
+      }
+    } catch (error) {
+      dispatch(hideLoading());
+    }
+  };
+
+  const columns = [
+    {
+      title: "name",
+      dataIndex: "name",
+      render: (text, record) => (
+        <p>{record.firstName}</p>
+      ),
+    },
+    {
+      title: "email",
+      dataIndex: "email",
+    },
+    {
+      title: "Phone",
+      dataIndex: "phone",
+    },
+    {
+      title: "status",
+      dataIndex: "status",
+    },
+    {
+      title: "actions",
+      dataIndex: "actions",
+      render: (text, record) => (
+        <div className="d-flex">
+          <em>Approve</em>
+        </div>
+      ),
+    },
+  ];
   return (
     <DashboardLayout>
-        Therapist List
+      <Table columns={columns} dataSource={therapist} />
     </DashboardLayout>
   );
 }
