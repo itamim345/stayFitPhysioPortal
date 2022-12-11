f1f0ac85703d070bc96c2e54815f2ce9f0d79a2c
AF Tamim <itamim345@gmail.com>
2022-12-11 21:16:29 +0530

    Added status changed route for user to doctor

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -38,4 +38,30 @@ router.get("/get-all-users", authmiddleware, async (req, res) => {
   }
 });
 
+// Post method to change doctor status
+router.get("/change-doctor-status", authmiddleware, async (req, res) => {
+  try {
+    const {therapistId, status, userId} = req.body;
+    const therapists = await therapist.findByIdAndUpdate();
+    return res.status(200).send({
+        message: "Therapist fetched Successfully!",
+        success: true,
+        data: therapist
+    })
+    const User = await user.findOne({_id: userId})
+    const unseenNotification = User.unseenNotification;
+    unseenNotification.push({
+      type: "New-therapist-request-changed",
+      message: `Your Therapist accoutn has been ${status}!`,
+      onClickPath: "/notfifications",
+    });
+    await user.findByIdAndUpdate(user._id, { unseenNotification });
+  } catch (error) {
+    return res.status(500).send({
+      message: "Error Occured in fetching users",
+      success: false,
+    });
+  }
+});
+
 module.exports = router;
