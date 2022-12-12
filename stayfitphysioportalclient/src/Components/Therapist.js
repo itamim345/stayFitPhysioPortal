b13604c2d70cd703865b4ff56b8b168605d03a44
AF Tamim <itamim345@gmail.com>
2022-12-13 02:23:34 +0530

    Added more details in therapist and added box shadow

--- stayfitphysioportalclient/src/Components/Therapist.js
+++ stayfitphysioportalclient/src/Components/Therapist.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import "../OurCss/common.css"
 
 export default function Therapist({therapist}) {
   return (
     <div>
-      <div className="border">
-        <div className="h6">
-          {therapist.firstName} {therapist.lastName}
-        </div>
+      <div className="border p-3 text-center single-therapist">
+        <h5 className='text-primary'>{therapist.firstName} {therapist.lastName}</h5>
+        <h5>{therapist.specialization}</h5>
+        <p><strong>Phone:</strong> {therapist.phone}</p>
+        <p><strong>Experience:</strong> {therapist.experience}</p>
+        <p><strong>Fees:</strong> {therapist.consaltancyFees}</p>
       </div>
     </div>
   );
