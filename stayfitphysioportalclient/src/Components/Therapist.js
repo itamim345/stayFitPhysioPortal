8f434b9de7693eff6b38dc4ca24ea365b09baada
AF Tamim <itamim345@gmail.com>
2022-12-12 21:27:17 +0530

    Added appproved therapist name into dashboard

--- stayfitphysioportalclient/src/Components/Therapist.js
+++ stayfitphysioportalclient/src/Components/Therapist.js
@@ -3,8 +3,10 @@ import React from 'react';
 export default function Therapist({therapist}) {
   return (
     <div>
-      <div>
-        <div className="h6">{therapist.firstName} {therapist.lastName}</div>
+      <div className="border">
+        <div className="h6">
+          {therapist.firstName} {therapist.lastName}
+        </div>
       </div>
     </div>
   );
--- stayfitphysioportalclient/src/Pages/Dashboard.js
+++ stayfitphysioportalclient/src/Pages/Dashboard.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from "axios";
 import { useEffect } from 'react';
 import DashboardLayout from '../Components/DashboardLayout';
+import { Col, Row } from 'antd';
+import Therapist from '../Components/Therapist';
+import { useDispatch, useSelector } from 'react-redux';
+import {showLoading, hideLoading} from './../Redux/alertReducers'
 
 export default function Dashboard() {
-
+  const [therapists, setTherapists] = useState([]);
+  const dispatch = useDispatch()
   const getData = async()=>{
     try {
+      dispatch(showLoading())
       const response = await axios.get("/api/user/get-all-approved-therapists", {
         headers : {
           Authorization : 'Bearer ' + localStorage.getItem('token')
         }
       })
-      console.log(response.data);
+      dispatch(hideLoading())
+      if(response.data.success){
+        setTherapists(response.data.data)
+      }
     } catch (error) {
+      dispatch(hideLoading())
       console.log(error);
     }
   }
@@ -23,6 +33,12 @@ export default function Dashboard() {
   }, []);
 
   return <DashboardLayout>
-      
+      <Row gutter={15}>
+          {therapists.map((therapist) => (
+            <Col span={8}>
+              <Therapist therapist={therapist}/>
+            </Col>
+          ))}
+      </Row>
     </DashboardLayout>
 }
