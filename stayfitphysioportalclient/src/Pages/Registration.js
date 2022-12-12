22665bbe4ca3706fafee7c3e16679010f4bda077
AF Tamim <itamim345@gmail.com>
2022-12-13 02:31:43 +0530

    added required in reg field

--- stayfitphysioportalclient/src/Pages/Registration.js
+++ stayfitphysioportalclient/src/Pages/Registration.js
@@ -49,17 +49,17 @@ export default function Registration() {
             </h2>
             <Form.Group className="mb-3" controlId="formBasicName">
               <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Your Name" name="name" />
+              <Form.Control type="text" placeholder="Your Name" name="name" required />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" name="email" />
+              <Form.Control type="email" placeholder="Enter email" name="email" required/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" name="password" />
+              <Form.Control type="password" placeholder="Password" name="password" required/>
             </Form.Group>
             <Button variant="primary" type="submit">
               Register
