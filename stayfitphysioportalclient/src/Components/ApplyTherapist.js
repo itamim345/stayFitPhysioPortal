fef6b3c72cf8bb302bf201a486b3cb3e6bf721a4
AF Tamim <itamim345@gmail.com>
2022-12-12 14:45:51 +0530

    Seperated the form into a different component and using in various place

--- stayfitphysioportalclient/src/Components/ApplyTherapist.js
+++ stayfitphysioportalclient/src/Components/ApplyTherapist.js
@@ -7,6 +7,7 @@ import { useNavigate } from 'react-router-dom';
 import DashboardLayout from './DashboardLayout';
 import { useSelector } from "react-redux";
 import { hideLoading, showLoading } from "../Redux/alertReducers";
+import TherapistForm from './TherapistForm';
 
 export default function ApplyTherapist() {
   const { user } = useSelector((state) => state.user);
@@ -48,107 +49,7 @@ export default function ApplyTherapist() {
             </h4>
             <hr className="hr-divider" />
           </div>
-          <Form layout="vertical" onFinish={handleSubmit}>
-            <h6>
-              <i className="text-decoration-underline">Personal Info:</i>
-            </h6>
-            <Row gutter={20}>
-              <Col span="8">
-                <Form.Item
-                  label="First Name"
-                  name="firstName"
-                  rules={[{ required: true }]}
-                >
-                  <Input placeholder="First Name..." />
-                </Form.Item>
-              </Col>
-              <Col span="8">
-                <Form.Item
-                  label="Last Name"
-                  name="lastName"
-                  rules={[{ required: true }]}
-                >
-                  <Input placeholder="Last Name..." />
-                </Form.Item>
-              </Col>
-              <Col span="8">
-                <Form.Item
-                  label="Language"
-                  name="language"
-                  rules={[{ required: true }]}
-                >
-                  <Input placeholder="Language..." />
-                </Form.Item>
-              </Col>
-            </Row>
-            <Row gutter={20}>
-              <Col span="8">
-                <Form.Item
-                  label="Phone"
-                  name="phone"
-                  rules={[{ required: true }]}
-                >
-                  <Input placeholder="Phone Number..." />
-                </Form.Item>
-              </Col>
-              <Col span="8">
-                <Form.Item
-                  label="Address"
-                  name="address"
-                  rules={[{ required: true }]}
-                >
-                  <Input placeholder="Address..." />
-                </Form.Item>
-              </Col>
-              <Col span="8">
-                <Form.Item
-                  label="Experience"
-                  name="experience"
-                  rules={[{ required: true }]}
-                >
-                  <Input placeholder="Experience Year..." />
-                </Form.Item>
-              </Col>
-            </Row>
-            <hr className="hr-divider" />
-            <h6>
-              <i className="text-decoration-underline">Professional Info:</i>
-            </h6>
-            <Row gutter={20}>
-              <Col span="8">
-                <Form.Item
-                  label="Specialization"
-                  name="specialization"
-                  rules={[{ required: true }]}
-                >
-                  <Input placeholder="Ex- Orthopedic, Pediatric, etc" />
-                </Form.Item>
-              </Col>
-              <Col span="8">
-                <Form.Item
-                  label="Fees"
-                  name="consaltancyFees"
-                  rules={[{ required: true }]}
-                >
-                  <Input placeholder="consultancyFees" />
-                </Form.Item>
-              </Col>
-              <Col span="8">
-                <Form.Item
-                  label="Timing"
-                  name="timing"
-                  rules={[{ required: true }]}
-                >
-                  <TimePicker.RangePicker />
-                </Form.Item>
-              </Col>
-            </Row>
-            <div>
-              <Button type="primary" htmlType="submit">
-                Apply
-              </Button>
-            </div>
-          </Form>
+          <TherapistForm onFinish={handleSubmit}/>
         </DashboardLayout>
       </div>
     );
---
+++ stayfitphysioportalclient/src/Components/TherapistForm.js
@@ -0,0 +1,106 @@
+import { Button, Col, Form, Input, Row, TimePicker } from 'antd';
+import React from 'react';
+
+export default function TherapistForm({handleSubmit}) {
+  return (
+    <div>
+      <Form layout="vertical" onFinish={handleSubmit}>
+        <h6>
+          <i className="text-decoration-underline">Personal Info:</i>
+        </h6>
+        <Row gutter={20}>
+          <Col span="8">
+            <Form.Item
+              label="First Name"
+              name="firstName"
+              rules={[{ required: true }]}
+            >
+              <Input placeholder="First Name..." />
+            </Form.Item>
+          </Col>
+          <Col span="8">
+            <Form.Item
+              label="Last Name"
+              name="lastName"
+              rules={[{ required: true }]}
+            >
+              <Input placeholder="Last Name..." />
+            </Form.Item>
+          </Col>
+          <Col span="8">
+            <Form.Item
+              label="Language"
+              name="language"
+              rules={[{ required: true }]}
+            >
+              <Input placeholder="Language..." />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={20}>
+          <Col span="8">
+            <Form.Item label="Phone" name="phone" rules={[{ required: true }]}>
+              <Input placeholder="Phone Number..." />
+            </Form.Item>
+          </Col>
+          <Col span="8">
+            <Form.Item
+              label="Address"
+              name="address"
+              rules={[{ required: true }]}
+            >
+              <Input placeholder="Address..." />
+            </Form.Item>
+          </Col>
+          <Col span="8">
+            <Form.Item
+              label="Experience"
+              name="experience"
+              rules={[{ required: true }]}
+            >
+              <Input placeholder="Experience Year..." />
+            </Form.Item>
+          </Col>
+        </Row>
+        <hr className="hr-divider" />
+        <h6>
+          <i className="text-decoration-underline">Professional Info:</i>
+        </h6>
+        <Row gutter={20}>
+          <Col span="8">
+            <Form.Item
+              label="Specialization"
+              name="specialization"
+              rules={[{ required: true }]}
+            >
+              <Input placeholder="Ex- Orthopedic, Pediatric, etc" />
+            </Form.Item>
+          </Col>
+          <Col span="8">
+            <Form.Item
+              label="Fees"
+              name="consaltancyFees"
+              rules={[{ required: true }]}
+            >
+              <Input placeholder="consultancyFees" />
+            </Form.Item>
+          </Col>
+          <Col span="8">
+            <Form.Item
+              label="Timing"
+              name="timing"
+              rules={[{ required: true }]}
+            >
+              <TimePicker.RangePicker />
+            </Form.Item>
+          </Col>
+        </Row>
+        <div>
+          <Button type="primary" htmlType="submit">
+            Apply
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+}
