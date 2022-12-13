b5cf417bbada247bde736fddc111f5d7fc3b0687
AF Tamim <itamim345@gmail.com>
2022-12-13 11:46:33 +0530

    modified css in single therapist

--- stayfitphysioportalclient/src/Components/Therapist.js
+++ stayfitphysioportalclient/src/Components/Therapist.js
@@ -5,10 +5,10 @@ export default function Therapist({therapist}) {
   return (
     <div>
       <div className="border p-3 text-center single-therapist">
-        <h5 className='text-primary'>{therapist.firstName} {therapist.lastName}</h5>
-        <h5>{therapist.specialization}</h5>
+        <h5 className='text-primary therapist-heading' >{therapist.firstName} {therapist.lastName}</h5>
+        <h5><em>{therapist.specialization}</em></h5>
         <p><strong>Phone:</strong> {therapist.phone}</p>
-        <p><strong>Experience:</strong> {therapist.experience}</p>
+        <p><strong>Experience:</strong> {therapist.experience} yrs.</p>
         <p><strong>Fees:</strong> {therapist.consaltancyFees}</p>
       </div>
     </div>
