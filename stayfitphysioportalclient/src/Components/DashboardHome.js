b62d62b034193fe4ff93e387e565a581f1899ae9
AF Tamim <itamim345@gmail.com>
2022-12-13 12:28:12 +0530

    Added home page img in dbhome component

--- stayfitphysioportalclient/src/Components/DashboardHome.js
+++ stayfitphysioportalclient/src/Components/DashboardHome.js
@@ -4,7 +4,9 @@ import DashboardLayout from './DashboardLayout';
 export default function DashboardHome() {
   return (
       <DashboardLayout>
-            
+        <div className='text-center mt-3'>
+            <img src="https://i.ibb.co/kc0yTB3/dashboard-img-removebg-preview.png" alt="dashboard-img" className='w-75 rounded' />
+        </div>
       </DashboardLayout>
   );
 }
