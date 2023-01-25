f6e599a637387a9eba4d2e652dae6fee0ccaf5bd
AF Tamim <itamim345@gmail.com>
2023-01-25 11:48:40 +0530

    Working with appointment booking

--- stayfitphysioportalclient/src/Pages/BookAppointment.js
+++ stayfitphysioportalclient/src/Pages/BookAppointment.js
@@ -9,10 +9,15 @@ import { hideLoading, showLoading } from "../Redux/alertReducers";
 import axios from "axios";
 import toast from "react-hot-toast";
 import moment from "moment";
+import "../OurCss/common.css"
+import { DatePicker, TimePicker } from "antd";
 
 export default function BookAppointment() {
     const {user} = useSelector((state) => state.user) 
     const [therapist, setTherapist] = useState(null)
+    const [isAvailable, setIsavailable] = useState(false);
+    const [date, setDate] = useState()
+    const [selectedTiming, setSelectedTiming] = useState()
     const params = useParams();
     const dispatch = useDispatch()
 
@@ -37,23 +42,41 @@ export default function BookAppointment() {
           console.log(response.data.data);
         }
       } catch (error) {
-
-
-
-
-
-
-
-
-
-
-
-
-
         console.log(error);
         dispatch(hideLoading());
       }
     };
+
+    const bookNow = async () => {
+       try {
+         dispatch(showLoading());
+         const response = await axios.post(
+           "/api/therapist/book-appoinment",
+           {
+             therapistId: params.therapistId,
+             userId: user._id,
+             date: date,
+             selectedTiming: selectedTiming,
+             therapistInfo: therapist,
+             userInfo : user
+           },
+           {
+             headers: {
+               Authorization: `Bearer ${localStorage.getItem("token")}`,
+             },
+           }
+         );
+
+         dispatch(hideLoading());
+         if (response.data.success) {
+           toast.success(response.data.message)
+         }
+       } catch (error) {
+         console.log(error);
+         toast.error("Something wrong here!");
+         dispatch(hideLoading());
+       } 
+    }
 
     useEffect(() => {
       getTherapistData();
@@ -61,8 +84,24 @@ export default function BookAppointment() {
   return (
     <DashboardLayout>
       {therapist && (
-        <div>
-          <h3>{therapist.firstName}</h3>
+        <div className="border p-3 single-therapist w-25">
+          <h3 className="text-primary text-center h5">
+            {therapist.firstName} {therapist.lastName}
+          </h3>
+          <hr />
+          <p>
+            <strong>Timing:</strong> {therapist.timing[0]} -{" "}
+            {therapist.timing[1]}
+          </p>
+          <div>
+            <DatePicker format="DD-MM-YYYY" className="mb-2 w-100" onClick={(value) =>setDate(moment(value).format("DD-MM-YYYY")) }/>
+            <TimePicker.RangePicker format="HH:mm" onChange={(values) => setSelectedTiming([
+                moment(values[0].format("HH:mm")),
+                moment(values[1].format("HH:mm"))
+            ])}/>
+          </div>
+          <button className="btn btn-primary btn-sm mt-2 w-100">Check Availability</button>
+          <button className="btn btn-danger btn-sm mt-2 w-100" onClick={bookNow}>BOOK NOW</button>
         </div>
       )}
     </DashboardLayout>
