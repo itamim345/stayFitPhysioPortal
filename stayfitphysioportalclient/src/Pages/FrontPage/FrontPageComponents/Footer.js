55fc616e8e5ee34d97b71662112fe6897638c309
AF Tamim <itamim345@gmail.com>
2023-01-27 15:38:01 +0530

    Modified Footer section

--- stayfitphysioportalclient/src/Pages/FrontPage/FrontPageComponents/Footer.js
+++ stayfitphysioportalclient/src/Pages/FrontPage/FrontPageComponents/Footer.js
@@ -1,10 +1,27 @@
 import React from 'react';
-
+import { Link } from "react-router-dom";
 export default function Footer() {
   return (
-    <div className='footer-area text-center text-white'>
-      <footer className='container py-3 mt-5'>
-        <p> &copy; All Right Reserved by <span>StayFit Physio Portal</span></p>
+    <div className="footer-area text-center text-white">
+      <footer className="container py-3 d-flex justify-content-between align-items-center">
+        <div>
+          &copy;2023 | All Right Reserved by{" "}
+          <span className="text-warning cursor-p">StayFit Physio Portal</span>
+        </div>
+        <div className="socical-links">
+          <a href="http://facebook.com" target="_blank" rel="noreferrer">
+            <i class="ri-facebook-box-line"></i>
+          </a>
+          <a href="http://gmail.com" target="_blank" rel="noreferrer">
+            <i class="ri-instagram-line"></i>
+          </a>
+          <a href="http://twitter.com" target="_blank" rel="noreferrer">
+            <i class="ri-twitter-line"></i>
+          </a>
+          <a href="http://linkedin.com" target="_blank" rel="noreferrer">
+            <i class="ri-linkedin-box-line"></i>
+          </a>
+        </div>
       </footer>
     </div>
   );
