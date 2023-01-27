a3ce1cdd7bff9fd4ca1f4c8c6bb80b64b7e2af4a
AF Tamim <itamim345@gmail.com>
2023-01-27 08:24:08 +0530

    whyPhysio section modified

--- stayfitphysioportalclient/src/Pages/FrontPage/FrontPageComponents/WhyPhysio.js
+++ stayfitphysioportalclient/src/Pages/FrontPage/FrontPageComponents/WhyPhysio.js
@@ -3,9 +3,25 @@ import React from 'react';
 export default function WhyPhysio() {
   return (
     <div>
-      <section className='why-physio container text-center py-3 my-3'>
-        <h2 className='mb-4'>Why <span className='text-primary'>Physio Therapy</span></h2>
-        <p>Physio Treatment otherwise called exercise based recuperation or kinesiology, assists with treating muscular, neurological, and cardiopulmonary infection conditions. With the assistance of PT, patients of all ages, including infants, children, adults, and the elderly, can be treated for their respective conditions. The trained allied health professionals who treat the disorder in a variety of ways are physiotherapists; thereby contributing to an improvement in the patients' overall quality of life.</p>
+      <section className="why-physio container pt-4 my-5 d-flex gap-5">
+        <div className='w-50'>
+          <img src="https://i.ibb.co/StmrNkn/why-Physio-img.jpg" alt="why-physio-img" className='w-100 rounded' />
+        </div>
+        <div className='w-50'>
+          <h2 className="mb-4">
+            Why <span className="text-primary">Physio Therapy</span>
+          </h2>
+          <p>
+            Physio Treatment otherwise called exercise based recuperation or
+            kinesiology, assists with treating muscular, neurological, and
+            cardiopulmonary infection conditions. With the assistance of PT,
+            patients of all ages, including infants, children, adults, and the
+            elderly, can be treated for their respective conditions. The trained
+            allied health professionals who treat the disorder in a variety of
+            ways are physiotherapists; thereby contributing to an improvement in
+            the patients' overall quality of life.
+          </p>
+        </div>
       </section>
     </div>
   );
