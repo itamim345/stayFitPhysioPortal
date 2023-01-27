42ccf4ef2f58df7292ab4366a21b69d746b56935
AF Tamim <itamim345@gmail.com>
2023-01-27 09:40:50 +0530

    About us section modified

--- stayfitphysioportalclient/src/Pages/FrontPage/FrontPageComponents/AboutUs.js
+++ stayfitphysioportalclient/src/Pages/FrontPage/FrontPageComponents/AboutUs.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import "../Frontpage.css"
 
 export default function AboutUs() {
   return (
-    <div>
-      <section className='about-us container py-3 my-3 text-center'>
-        <h2>About <span className='text-primary'>Us</span></h2>
-        <p>We are an experience team Having a platform for the Patient and Therapist, We Ensure a excellent hassle free modern systemetic way to manage the patient and therapist problem. In this system a patient can get the appointment with any of the therapist from our platform based his choice and a therapist can apply as a therapist anytime.</p>
+    <div id="about-us-section">
+      <section className="about-us container d-flex gap-5 align-items-center">
+        <div className="w-50">
+          <h2 className="mb-4">
+            About <span className="text-primary">Us</span>
+          </h2>
+          <p>
+            We are an experience team Having a platform for the Patient and
+            Therapist, We Ensure a excellent hassle free modern systemetic way
+            to manage the patient and therapist problem. In this system a
+            patient can get the appointment with any of the therapist from our
+            platform based on his choice and a therapist can apply as a therapist
+            anytime.
+          </p>
+        </div>
+        <div className="w-50">
+          <img
+            src="https://i.ibb.co/QDVCmCB/physio-Portal-about-us.jpg"
+            alt="why-physio-img"
+            className="w-100 rounded"
+          />
+        </div>
       </section>
     </div>
   );
